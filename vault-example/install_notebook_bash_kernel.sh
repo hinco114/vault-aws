70cf4f7ad7af204da4636fcae6c451b7873e34b7
SangHyoun Yun <hainco@gmail.com>
2026-03-09 00:55:49 +0900

    Bash 쉘 설정 업데이트

--- vault-example/install_notebook_bash_kernel.sh
+++ vault-example/install_notebook_bash_kernel.sh
@@ -1,3 +1,6 @@
+# 파일 위치로 이동
+pushd $(dirname "$0")
+
 # venv 활성화
 python3 -m venv .venv
 source .venv/bin/activate
@@ -5,3 +8,6 @@ source .venv/bin/activate
 # bash_kernel 설치
 pip install bash_kernel 
 python -m bash_kernel.install 
+
+echo "다음 명령어를 실행하여 venv 활성화"
+echo "source ./vault-example/.venv/bin/activate"
